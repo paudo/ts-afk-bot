19b38d3bdd19bd61015591b576f5945f13ae832a
Dominic Pauli <dominic.pauli@ericsson.com>
2022-02-08 09:03:15 +0100

    added console input options (pause/restart)

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,37 @@ import {Worker} from 'worker_threads';
 let stdin = process.openStdin();
 process.stdin.setRawMode(true);
 
-console.log('Press q to quit the program');
+console.log('Press:\tq to quit the program\n' +
+  '\tp to pause mouse movement\n' +
+  '\tr to restart mouse movement');
 stdin.resume();
+const workerPath = './out/mouseMovement.js';
 
-let worker = new Worker('./out/mouseMovement.js');
+let worker = new Worker(workerPath);
 stdin.on('data', function (keydata) {
   if (keydata == 'q') {
     console.log('Terminating Worker.');
     worker.terminate().then(process.exit(0));
+  } else if (keydata == 'p') {
+    if (worker.threadId > 0) {
+      worker.terminate().then();
+      write('Paused mouse movement.');
+    } else {
+      write('Mouse movement already paused.');
+    }
+  } else if (keydata == 'r') {
+    if (worker.threadId < 1) {
+      worker = new Worker(workerPath);
+      write('Restarted mouse movement.');
+    } else {
+      write('Mouse movement is not paused.');
+    }
   }
 });
+
+function write(message: string) {
+  process.stdout.cursorTo(0);
+  process.stdout.clearLine(0);
+  process.stdout.write(message);
+
+}
