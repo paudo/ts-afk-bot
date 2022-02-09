54eb9f887f62e2c973f6a59a66e8a2a897a78d7e
Dominic Pauli <dominic.pauli@ericsson.com>
2022-02-09 09:33:16 +0100

    converted mouseMovement to class

--- src/mouseMovement.ts
+++ src/mouseMovement.ts
@@ -1,21 +1,33 @@
-// @ts-ignore
-import {mouse, left, right, up, down, straightTo, centerOf, Region, sleep} from '@nut-tree/nut-js';
+import {mouse, left, right, up, down, sleep} from '@nut-tree/nut-js';
 
-async function square() {
-  await mouse.move(right(move));
-  await mouse.move(down(move));
-  await mouse.move(left(move));
-  await mouse.move(up(move));
-}
+class MouseMovement {
 
-const [delay, move] = [30000, 100];
-let lastPosition = await mouse.getPosition();
+  private delay = 30000;
+  private move = 100;
 
-while (true) {
-  const tmpPosition = await mouse.getPosition();
-  if (lastPosition.x === tmpPosition.x && lastPosition.y === tmpPosition.y) {
-    await square();
+  constructor() {
+    this.startMovement().then();
   }
-  lastPosition = await mouse.getPosition();
-  await sleep(delay);
+
+  private async startMovement() {
+    let lastPosition = await mouse.getPosition();
+
+    while (true) {
+      const tmpPosition = await mouse.getPosition();
+      if (lastPosition.x === tmpPosition.x && lastPosition.y === tmpPosition.y) {
+        await this.square();
+      }
+      lastPosition = await mouse.getPosition();
+      await sleep(this.delay);
+    }
+  }
+
+  private async square() {
+    await mouse.move(right(this.move));
+    await mouse.move(down(this.move));
+    await mouse.move(left(this.move));
+    await mouse.move(up(this.move));
+  }
 }
+
+export default new MouseMovement();
